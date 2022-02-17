149d6383ecd0b957707db0370d182c974205e97b
carlosamr2 <caraumon@espol.edu.ec>
2022-02-17 11:11:07 -0500

    sponsor format fix

--- sponsor/forms.py
+++ sponsor/forms.py
@@ -1,7 +1,7 @@
 from crispy_forms.helper import FormHelper
 from django import forms
 from .models import *
-
+from crispy_forms.layout import Column, Div, Field, Layout, Row
 class SponsorForm(forms.ModelForm):
     class Meta:
         model= Sponsor
@@ -9,15 +9,31 @@ class SponsorForm(forms.ModelForm):
             "imagen": "Logo del sponsor",
             "descripcion": "descripcion"
         }
+        fields = ('codigo','nombre', 'descripcion', 'telefono', 'nombre_contacto', 'url','imagen','fecha_inicio','fecha_fin')
+
         widgets = {
             "imagen": forms.ClearableFileInput(),
-            "descripcion":forms.Textarea(),
-            "fecha_inicio": forms.SelectDateWidget(),
-            "fecha_fin": forms.SelectDateWidget()
+            "descripcion":forms.Textarea(attrs={'rows':4, 'cols':15}),
+            "fecha_inicio": forms.SelectDateWidget(attrs={'style': 'display: inline-block; width: 33%;'}),
+            "fecha_fin": forms.SelectDateWidget(attrs={'style': 'display: inline-block; width: 33%;'})
         }
-        fields = ('codigo','nombre', 'descripcion', 'telefono', 'nombre_contacto', 'url','imagen','fecha_inicio','fecha_fin')
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.disable_csrf = True
         self.helper.form_tag = False
+        self.helper.layout = Layout(
+            Row(
+                Column('codigo', css_class='col-6'),
+                Column('nombre', css_class='col-6'),
+                Column('descripcion', css_class='col-6'),
+                Column('telefono', css_class='col-6'),
+                Column('nombre_contacto', css_class='col-6'),
+            ),
+            Row(
+                Column('url', css_class='col-6'),
+                Column('imagen', css_class='col-6'),
+                Column('fecha_inicio', css_class='col-6'),
+                Column('fecha_fin', css_class='col-6 '),
+            ),
+        )
