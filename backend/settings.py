4f1fe633129326c264bcfaa536d526f5f8bc260e
GeorgHenriquez <ghenriqu@espol.edu.ec>
2022-03-11 01:58:59 -0500

    almacenamiento anadido a opciones de admin

--- backend/settings.py
+++ backend/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     # 'novagym.cliente',
     'membresia',
     'calendario',
+    'almacenamiento',
 ]
 
 MIDDLEWARE = [
--- calendario/urls.py
+++ calendario/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 from django.urls import include, path
 from calendario.views import *
 
-from comunidad.urls import comunidad_api
 
 urlpatterns = [
     path('',ShowCalendario.as_view(),name="calendario")
