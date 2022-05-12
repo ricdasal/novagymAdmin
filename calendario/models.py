342c4f33ccaa9b250c2d16076c15f46685869a43
Carlos Moncayo <carlosamr2@hotmail.com>
2022-05-12 10:57:32 -0500

    fix 4

--- calendario/models.py
+++ calendario/models.py
@@ -160,7 +160,7 @@ class MaquinaReserva(models.Model):
     id=models.AutoField(primary_key=True)
     codigo=models.CharField(max_length=20, unique=True, default=generarCodigo, editable=False)
     maquina=models.ForeignKey(Maquina,on_delete=models.PROTECT)
-    horario_id=models.ForeignKey(HorarioMaquina,on_delete=models.PROTECT)
+    horario=models.ForeignKey(HorarioMaquina,on_delete=models.PROTECT)
     fecha= models.DateField()
     posicion=models.ForeignKey(PosicionMaquina, on_delete=models.PROTECT)
     usuario=models.ForeignKey(UserDetails,on_delete=models.PROTECT)
